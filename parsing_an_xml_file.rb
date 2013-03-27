f6a52ce28adaf550ed1c9b3a8a26736c0d6cb930
TaniaBosco <taniabosco.r@gmail.com>
2013-03-27 17:39:10 +0530

    Ruby task for parsing xml file

---
+++ parsing_an_xml_file.rb
@@ -0,0 +1,44 @@
+require 'rexml/document'
+require 'mysql'
+include REXML
+
+mysql = Mysql.new("localhost", "root", "", "diasdb")
+xmlfile = File.new("sitemap.xml")
+xmldoc = Document.new(xmlfile)
+puts("the given xml file is parsed and the result is ")
+puts(xmldoc)   #the parsed xml file is displayed.
+
+
+mysql.query("drop table urlset")
+mysql.query("create table if not exists urlset(id int auto_increment primary key,location varchar(70),priority varchar(10))")
+
+# for each element in */url in the xml file
+
+xmldoc.elements.each("urlset/url") do |e|  
+	
+	@loc=e.elements["loc"].text.to_s
+
+	@priority=e.elements["priority"].text.to_s
+	
+	# inserting into table urlset 
+	mysql.query("insert into urlset (location,priority)values('#{@loc}','#{@priority}')")
+	
+end
+
+
+# displaying the table urlset from database
+
+puts "Location and Priority are added to mysql table \"urlset\""
+puts "Location       Priority"
+result = mysql.query("select * from urlset ")
+result.each_hash do |f|
+    puts "#{f['location']}    #{f['priority']}"
+    puts ""
+  end	
+
+
+
+	
+
+
+
